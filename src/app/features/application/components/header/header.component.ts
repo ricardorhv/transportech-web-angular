c03a0cdffda5bb9dc227215f6651292f9cdf3ae9
Ricardo Vinciguerra <ricardorhv.dev@gmail.com>
2025-05-30 20:57:10 -0300

    styles: add sign out link

--- src/app/features/application/components/header/header.component.ts
+++ src/app/features/application/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideLogOut } from '@ng-icons/lucide';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './header.component.html',
+  viewProviders: [provideIcons({ lucideLogOut })],
 })
 export class HeaderComponent {}
