28c00bf1a0bc9a9126f60be265297b2b0a59e0ec
Ricardo Vinciguerra <ricardorhv.dev@gmail.com>
2025-05-30 01:16:56 -0300

    styles: create sign-in and sign-up pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './features/auth/pages/sign-in/sign-in.component';
+import { SignUpComponent } from './features/auth/pages/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth/sign-in',
+    component: SignInComponent,
+  },
+  {
+    path: 'auth/sign-up',
+    component: SignUpComponent,
+  },
+];
