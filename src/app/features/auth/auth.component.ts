0356325ff04f7a6e8975cf994a425024148957b0
Ricardo Vinciguerra <ricardorhv.dev@gmail.com>
2025-06-06 14:02:39 -0300

    styles: improve responsive

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'app-auth',
   imports: [RouterOutlet],
   templateUrl: './auth.component.html',
+  providers: [provideIcons({})],
 })
 export class AuthComponent {}
